9996f4ccec84fc33a4c6953279bcbfc3487cbbe3
Aleksei Parygin <am.parygin@gmail.com>
2024-10-01 23:21:20 +0300

    fix cli.py, brain_games.py add flake8 setup.cfg

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -5,13 +5,13 @@ from brain_games import cli
 
 
 def greet():
-	print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
 
 
 def main():
-	greet()
-	cli.welcome_user()
+    greet()
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
-	main()
+    main()
