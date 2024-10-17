279ea142a82952fd826cf1026d927c4ebb2e6ce4
Aleksei Parygin <am.parygin@gmail.com>
2024-10-17 23:53:27 +0300

    fix engine.py, remove cli.py

--- brain_games/scripts/engine.py
+++ brain_games/scripts/engine.py
@@ -1,17 +1,23 @@
 import random
 import prompt
-from brain_games import cli
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
+
 
 def checker(player_answer, correct_answer):
     if player_answer == correct_answer:
         print('Correct!')
         return True
     else:
-        print(f"'{player_answer}' is a wrong answer ;(. Correct answer was '{correct_answer}'. Try again!")
         return False
 
 def game_engine(game_name, welcome_message, check_func=checker):
-    name = cli.welcome_user()
+    name = welcome_user()
     print(welcome_message)
     num_of_rounds = 3
     for _ in range(num_of_rounds):
@@ -19,6 +25,7 @@ def game_engine(game_name, welcome_message, check_func=checker):
         print(f'Question: {expression}')
         player_answer = prompt.string('Your answer: ').strip().lower()
         if not checker(player_answer, correct_answer):
+            print(f"'{player_answer}' is a wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
             break
     else:
         print(f'Congratulations, {name}!')
